dfa25b6d10bcf30343847555d19c91bf8a88f9c0
Martin Stoev <martin.y.stoev@gmail.com>
2023-10-31 10:28:50 +0200

    fix tests

--- src/pages/product/product.test.js
+++ src/pages/product/product.test.js
@@ -39,6 +39,52 @@ describe('product page', () => {
     );
 
     expect(container).toMatchInlineSnapshot(`
+      .c3 {
+        font-size: 0.75em;
+        width: 80%;
+      }
+
+      .c3 h3 {
+        margin-bottom: 10px;
+      }
+
+      .c3 fieldset {
+        width: 100%;
+      }
+
+      .c3 input {
+        height: 24px;
+      }
+
+      .c3 textarea {
+        height: 120px;
+      }
+
+      .c3 button {
+        margin-top: 5px;
+      }
+
+      .c1 .image-gallery-original-style {
+        height: 640px;
+        border-radius: 3px;
+        background-color: #1C1311;
+      }
+
+      .c1 .image-gallery-original-style img {
+        height: 100%;
+        object-fit: contain;
+      }
+
+      .c1 .image-gallery-thumbnail-style img {
+        width: 100%;
+        height: 100px;
+        object-fit: cover;
+      }
+
+      .c1 .image-gallery {
+        flex-basis: 70%;
+      }
+
       .c0 {
         width: 90%;
         margin: 20px auto;
@@ -47,113 +93,71 @@ describe('product page', () => {
         gap: 10px;
       }
 
-      .c0 .image-gallery-original-style {
-        height: 640px;
-        border-radius: 3px;
-        background-color: #1C1311);
+      .c0 a {
+        color: #1C1311;
       }
 
-      .c0 .image-gallery-original-style img {
-        height: 100%;
-        object-fit: contain;
-      }
-
-      .c0 .image-gallery-thumbnail-style img {
-        width: 100%;
-        height: 100px;
-        object-fit: cover;
-      }
-
-      .c0 .image-gallery {
-        flex-basis: 70%;
-      }
-
-      .c0 .product-details {
+      .c2 {
         flex-basis: 30%;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
       }
 
-      .c0 a {
-        color: #1C1311;
-      }
-
-      .c0 .product-query-container {
-        font-size: 0.75em;
-        width: 80%;
-      }
-
-      .c0 .product-query-container h3 {
-        margin-bottom: 10px;
-      }
-
-      .c0 .product-query-container fieldset {
-        width: 100%;
-      }
-
-      .c0 .product-query-container input {
-        height: 24px;
-      }
-
-      .c0 .product-query-container textarea {
-        height: 120px;
-      }
-
-      .c0 .product-query-container button {
-        margin-top: 5px;
+      @media only screen and (max-width:768px) {
+        .c3 {
+          width: 100%;
+        }
       }
 
       @media only screen and (max-width:768px) {
         .c0 {
           flex-direction: column;
         }
-
-        .c0 .product-query-container {
-          width: 100%;
-        }
       }
 
       <div>
         <div>
-          <div>
-             
-            test-name
-          </div>
+           
+          test-name
+        </div>
+        <div
+          class="c0"
+        >
           <div
-            class="c0"
+            class="c1"
           >
             <div>
               ImageGallery
             </div>
-            <div
-              class="product-details"
-            >
-              <div>
-                <p>
-                  Име: 
-                  test-name
-                </p>
-                <a
-                  href="/category/test-category"
-                >
-                  <p>
-                    Категория: 
-                  </p>
-                </a>
-                <p>
-                  Описание: 
-                </p>
-              </div>
-              <div
-                class="product-query-container"
+          </div>
+          <div
+            class="c2"
+          >
+            <div>
+              <p>
+                Име: 
+                test-name
+              </p>
+              <a
+                href="/category/test-category"
               >
-                <h3>
-                  Изпрати запитване за този продукт
-                </h3>
-                <div>
-                  QueryForm
-                </div>
+                <p>
+                  Категория: 
+                </p>
+              </a>
+              <p>
+                Описание: 
+              </p>
+            </div>
+            <div
+              class="c3"
+            >
+              <h3>
+                Изпрати запитване за този продукт
+              </h3>
+              <div>
+                QueryForm
               </div>
             </div>
           </div>
@@ -180,6 +184,52 @@ describe('product page', () => {
     );
 
     expect(container).toMatchInlineSnapshot(`
+      .c3 {
+        font-size: 0.75em;
+        width: 80%;
+      }
+
+      .c3 h3 {
+        margin-bottom: 10px;
+      }
+
+      .c3 fieldset {
+        width: 100%;
+      }
+
+      .c3 input {
+        height: 24px;
+      }
+
+      .c3 textarea {
+        height: 120px;
+      }
+
+      .c3 button {
+        margin-top: 5px;
+      }
+
+      .c1 .image-gallery-original-style {
+        height: 640px;
+        border-radius: 3px;
+        background-color: #1C1311;
+      }
+
+      .c1 .image-gallery-original-style img {
+        height: 100%;
+        object-fit: contain;
+      }
+
+      .c1 .image-gallery-thumbnail-style img {
+        width: 100%;
+        height: 100px;
+        object-fit: cover;
+      }
+
+      .c1 .image-gallery {
+        flex-basis: 70%;
+      }
+
       .c0 {
         width: 90%;
         margin: 20px auto;
@@ -188,114 +238,72 @@ describe('product page', () => {
         gap: 10px;
       }
 
-      .c0 .image-gallery-original-style {
-        height: 640px;
-        border-radius: 3px;
-        background-color: #1C1311);
+      .c0 a {
+        color: #1C1311;
       }
 
-      .c0 .image-gallery-original-style img {
-        height: 100%;
-        object-fit: contain;
-      }
-
-      .c0 .image-gallery-thumbnail-style img {
-        width: 100%;
-        height: 100px;
-        object-fit: cover;
-      }
-
-      .c0 .image-gallery {
-        flex-basis: 70%;
-      }
-
-      .c0 .product-details {
+      .c2 {
         flex-basis: 30%;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
       }
 
-      .c0 a {
-        color: #1C1311;
-      }
-
-      .c0 .product-query-container {
-        font-size: 0.75em;
-        width: 80%;
-      }
-
-      .c0 .product-query-container h3 {
-        margin-bottom: 10px;
-      }
-
-      .c0 .product-query-container fieldset {
-        width: 100%;
-      }
-
-      .c0 .product-query-container input {
-        height: 24px;
-      }
-
-      .c0 .product-query-container textarea {
-        height: 120px;
-      }
-
-      .c0 .product-query-container button {
-        margin-top: 5px;
+      @media only screen and (max-width:768px) {
+        .c3 {
+          width: 100%;
+        }
       }
 
       @media only screen and (max-width:768px) {
         .c0 {
           flex-direction: column;
         }
-
-        .c0 .product-query-container {
-          width: 100%;
-        }
       }
 
       <div>
         <div>
-          <div>
-             
-            test-name
-          </div>
+           
+          test-name
+        </div>
+        <div
+          class="c0"
+        >
           <div
-            class="c0"
+            class="c1"
           >
             <div>
               ImageGallery
             </div>
-            <div
-              class="product-details"
-            >
-              <div>
-                <p>
-                  Име: 
-                  test-name
-                </p>
-                <a
-                  href="/category/test-category"
-                >
-                  <p>
-                    Категория: 
-                  </p>
-                </a>
-                <p>
-                  Описание: 
-                  test-description
-                </p>
-              </div>
-              <div
-                class="product-query-container"
+          </div>
+          <div
+            class="c2"
+          >
+            <div>
+              <p>
+                Име: 
+                test-name
+              </p>
+              <a
+                href="/category/test-category"
               >
-                <h3>
-                  Изпрати запитване за този продукт
-                </h3>
-                <div>
-                  QueryForm
-                </div>
+                <p>
+                  Категория: 
+                </p>
+              </a>
+              <p>
+                Описание: 
+                test-description
+              </p>
+            </div>
+            <div
+              class="c3"
+            >
+              <h3>
+                Изпрати запитване за този продукт
+              </h3>
+              <div>
+                QueryForm
               </div>
             </div>
           </div>
