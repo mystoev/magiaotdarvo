f56b3fe565571d91c86ef7a904a61254488c67c8
Martin Stoev <martin.y.stoev@gmail.com>
2023-11-07 13:23:34 +0200

    fix upload snapshots

--- src/pages/admin/upload.test.js
+++ src/pages/admin/upload.test.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import { fireEvent, waitFor } from '@testing-library/react';
+import React from 'react';
 import { renderWithTheme } from '../../../test/utils';
 
 import Upload from './upload';
@@ -8,7 +8,48 @@ describe('<Upload /> tests', () => {
   it('should render', () => {
     const { container } = renderWithTheme(<Upload />);
 
-    expect(container).toMatchSnapshot();
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        display: inline-block;
+        background: #ffffff;
+        position: relative;
+        height: 200px;
+      }
+
+      .c0 img {
+        height: 100%;
+        width: auto;
+      }
+
+      .c0 input {
+        display: block;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        opacity: 0;
+        cursor: pointer;
+      }
+
+      <div>
+        <div
+          class="c0"
+        >
+          <img
+            src="[object Object]"
+          />
+          <input
+            data-testid="file-input"
+            multiple=""
+            title="Добави снимка"
+            type="file"
+          />
+        </div>
+      </div>
+    `);
   });
 
   it('should upload', async () => {
