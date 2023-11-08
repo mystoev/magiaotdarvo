24174f88021dff676b8e02b2efd4c5ba6a9e3dd8
Martin Stoev <martin.y.stoev@gmail.com>
2023-11-08 14:13:44 +0200

    refactor and test admin selectors

--- src/pages/admin/selectors.js
+++ src/pages/admin/selectors.js
@@ -3,27 +3,24 @@ import React from 'react';
 import { Thumbnail } from '../category/thumbnail';
 
 export const selectProducts = (data, selectedCategory) => {
-  const products =
-    data &&
-    data.map((d) => (
+  if (!data) {
+    return [];
+  }
+
+  return [
+    <Thumbnail
+      key={`new-${selectedCategory}`}
+      data={{ name: 'Добави нов' }}
+      category={selectedCategory}
+      link={`/admin/add/${selectedCategory}`}
+    />,
+    ...data.map((d) => (
       <Thumbnail
         key={d.folder}
         data={d}
         category={selectedCategory}
         link={`/admin/edit/${selectedCategory}/${d.folder}`}
       />
-    ));
-
-  if (products) {
-    products.push(
-      <Thumbnail
-        key={`new-${selectedCategory}`}
-        data={{ name: 'Добави нов' }}
-        category={selectedCategory}
-        link={`/admin/add/${selectedCategory}`}
-      />
-    );
-  }
-
-  return products;
+    ))
+  ];
 };
---
+++ src/pages/admin/selectors.test.js
@@ -0,0 +1,61 @@
+import { selectProducts } from './selectors';
+
+describe('products selectors', () => {
+  it('should be empty without data', () => {
+    const result = selectProducts(null, null);
+
+    expect(result).toEqual([]);
+  });
+
+  it('should all thumbnails with one more to add new', () => {
+    const category = 'mock-category';
+    const data = [
+      { folder: 'test-folder-1' },
+      { folder: 'test-folder-2' },
+      { other: 'no-to-render' }
+    ];
+
+    const result = selectProducts(data, category);
+    expect(result).toHaveLength(data.length + 1);
+    expect(result).toMatchInlineSnapshot(`
+      [
+        <Thumbnail
+          category="mock-category"
+          data={
+            {
+              "name": "Добави нов",
+            }
+          }
+          link="/admin/add/mock-category"
+        />,
+        <Thumbnail
+          category="mock-category"
+          data={
+            {
+              "folder": "test-folder-1",
+            }
+          }
+          link="/admin/edit/mock-category/test-folder-1"
+        />,
+        <Thumbnail
+          category="mock-category"
+          data={
+            {
+              "folder": "test-folder-2",
+            }
+          }
+          link="/admin/edit/mock-category/test-folder-2"
+        />,
+        <Thumbnail
+          category="mock-category"
+          data={
+            {
+              "other": "no-to-render",
+            }
+          }
+          link="/admin/edit/mock-category/undefined"
+        />,
+      ]
+    `);
+  });
+});
